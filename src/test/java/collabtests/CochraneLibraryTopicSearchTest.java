892d4eef2e2c2c12266ac6d0fc021beca2a3d7c4
Douglas Raymond Siano <DouglasSiano@gmail.com>
2020-03-23 20:07:36 -0400

    Cardigans skeles

---
+++ src/test/java/collabtests/CochraneLibraryTopicSearchTest.java
@@ -0,0 +1,55 @@
+package collabtests;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.rubberdougie.collab.Cardigans;
+import org.rubberdougie.collab.GlobalPageMethods;
+
+public class CochraneLibraryTopicSearchTest extends GlobalPageMethods {
+
+	private WebDriverWait wait;
+	private Cardigans cardigans;
+
+	@Before
+	public void beforeFunction() {
+		driver.manage().window().setSize(new Dimension(900, 900));
+		driver.navigate().to("https://www.cochranelibrary.com/");
+
+		cardigans = new Cardigans(driver);
+
+		assertTrue(cardigans.isInitialized());
+	}
+
+	@Test
+	public void testTopicSearchButton() {
+
+		/*
+		 * Given
+		 */
+
+		/*
+		 * When
+		 */
+
+		wait = new WebDriverWait(driver, 30);
+		wait.until(ExpectedConditions.visibilityOf(cardigans.getGastroSearchButton()));
+		CochraneLibraryTopicSearchResult cochraneLibraryTopicSearchResult = cardigans.gastroSearch();
+
+		/*
+		 * Then
+		 */
+
+		wait = new WebDriverWait(driver, 30);
+		wait.until(ExpectedConditions.attributeContains(cochraneLibraryTopicSearchResult.getCochraneReviewsTab(),
+				"class", "tab"));
+		assertTrue(cochraneLibraryTopicSearchResult.isInitialized());
+
+		assertEquals("Gastroenterology & hepatology", cochraneLibraryTopicSearchResult.getTopicTitle1().getText());
+	}
+}
---
+++ src/test/java/org/rubberdougie/collab/Cardigans.java
@@ -0,0 +1,31 @@
+package org.rubberdougie.collab;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class Cardigans extends PageObject {
+
+	@FindBy(xpath = "/html/body/div[1]/div[4]/div[1]/div[2]/div[1]/div/div/div[2]/section/div[1]/div/div/div[1]/div/div/div[2]/div[1]/div/section/div[1]/div/div/div/div[2]/div/div[2]/dl/dd[1]/ul/li[1]/a/button")
+	private WebElement gastroSearchButton;
+
+	@FindBy(xpath = "/html/head/meta[2]")
+	private WebElement url;
+
+	public Cardigans(WebDriver driver) {
+		super(driver);
+	}
+
+	public boolean isInitialized() {
+		return url.getAttribute("content").contains("https://www.cochranelibrary.com");
+	}
+
+	public CochraneLibraryTopicSearchResult gastroSearch() {
+		gastroSearchButton.click();
+		return new CochraneLibraryTopicSearchResult(driver);
+	}
+
+	public WebElement getGastroSearchButton() {
+		return gastroSearchButton;
+	}
+}
